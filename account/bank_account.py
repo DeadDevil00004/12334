8327573a0390bca05a4fbdbcc545f91ff96227ea
DeadDevil00004 <deaddevil00004@gmail.com>
2025-04-26 12:19:56 +0530

    Add files via upload

---
+++ account/bank_account.py
@@ -0,0 +1,63 @@
+
+from account.Transaction import Transaction
+from account.user import User
+
+class BankAccount:
+    def __init__(self, name="John", email="john@gmail.com", initial_balance=0):
+        if not isinstance(initial_balance, (int, float)) or initial_balance < 0:
+            raise ValueError("Invalid initial balance!")
+        self.balance = initial_balance
+        self.transactions_history = []
+        self.account_type = "Generic"
+        self.user = User(name, email)
+
+    def deposit(self, amount):
+        if not isinstance(amount, (int, float)) or amount <= 0:
+            raise ValueError("Deposit amount is invalid!")
+        self.balance += amount
+        self.transactions_history.append(Transaction(amount, "deposit"))
+
+    def withdraw(self, amount):
+        if not isinstance(amount, (int, float)) or amount <= 0:
+            raise ValueError("Withdrawal amount is invalid!")
+        if self.balance < amount:
+            raise ValueError("Insufficient balance!")
+        self.balance -= amount
+        self.transactions_history.append(Transaction(amount, "withdraw"))
+
+    def get_balance(self):
+        return self.balance
+
+    def get_transaction_history(self):
+        return self.transactions_history
+
+    def get_account_type(self):
+        return self.account_type
+
+    def get_user(self):
+        return self.user
+
+
+class SavingsAccount(BankAccount):
+    MIN_BALANCE = 100
+
+    def withdraw(self, amount):
+        if self.balance - amount < self.MIN_BALANCE:
+            raise ValueError(f"Cannot withdraw. Minimum balance of Rs.{self.MIN_BALANCE} required.")
+        super().withdraw(amount)
+
+    def get_account_type(self):
+        return "Savings account"
+
+class CurrentAccount(BankAccount):
+    def get_account_type(self):
+        return "Current account"
+
+class StudentAccount(BankAccount):
+    def withdraw(self, amount):
+        if self.balance - amount < 100:
+            raise ValueError("A minimum balance of Rs.100 needed to withdraw from a Student account!")
+        super().withdraw(amount)
+
+    def get_account_type(self):
+        return "Students account"
